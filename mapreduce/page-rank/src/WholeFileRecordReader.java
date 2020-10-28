20af9cae406172529f038fa0ddca2435c785da95
Kevin Del Castillo <quebin31@gmail.com>
2020-10-28 16:16:40 -0500

    refactor: move MapReduce jobs to subfolder

--- mapreduce/page-rank/src/WholeFileRecordReader.java
+++ mapreduce/page-rank/src/WholeFileRecordReader.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
