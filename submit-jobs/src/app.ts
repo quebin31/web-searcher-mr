987d34974b469c52f4cd182462f751cb3ae76ea5
Kevin Del Castillo <quebin31@gmail.com>
2020-10-28 18:54:28 -0500

    refactor: renamed project, only submit jobs

--- submit-jobs/src/app.ts
+++ submit-jobs/src/app.ts
@@ -12,50 +12,8 @@ interface Options {
     region: string,
     clusterName: string,
     pageRankIterations: string,
-    timeout: number,
 }
 
-function sleep(milliseconds: number) {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
-}
-
-async function waitForJob(options: Options, jobClient: any, jobResp: any): Promise<string> {
-    const terminalStates = new Set(['DONE', 'ERROR', 'CANCELLED']);
-    const start = (new Date()).getMilliseconds();
-
-    const jobReq = {
-        projectId: options.projectId,
-        region: options.region,
-        jobId: jobResp.reference.jobId,
-    };
-
-    while (!terminalStates.has(jobResp.status.state)) {
-        let now = (new Date()).getMilliseconds();
-        if (now - options.timeout > start) {
-            await jobClient.cancelJob(jobReq);
-
-            console.log(`Job ${jobResp.reference.jobId} timed out after threshold of ${options.timeout / 60 * 1000} minutes`);
-        }
-
-        await sleep(1000);
-        [jobResp] = await jobClient.getJob(jobReq);
-    }
-
-    return jobResp.status.state;
-}
-
-async function deleteCluster(options: Options, clusterClient: any) {
-    const [deleteOperation] = await clusterClient.deleteCluster({
-        projectId: options.projectId,
-        region: options.region,
-        clusterName: options.clusterName,
-    });
-
-    console.log(`Deleting cluster`);
-    await deleteOperation.promise();
-
-    console.log(`Cluster deleted succesfully`);
-}
 
 async function main(options: Options) {
     console.log('Received the following options:', options);
@@ -74,7 +32,7 @@ async function main(options: Options) {
     const configClusterBucket = `${options.clusterName}-config`;
     const buckets = await storage.getBuckets();
     if (!buckets[0].some((bucket) => bucket.name === configClusterBucket)) {
-        console.log(`Creating bucket ${configClusterBucket} for this cluster`);
+        console.log(`Creating config bucket ${configClusterBucket} for this cluster`);
         await storage.createBucket(configClusterBucket, {
             location: options.region,
             regional: true,
@@ -82,13 +40,13 @@ async function main(options: Options) {
         });
     }
 
-
     const [createOperation] = await clusterClient.createCluster({
         projectId: options.projectId,
         region: options.region,
         cluster: {
             clusterName: options.clusterName,
             config: {
+                configBucket: configClusterBucket,
                 softwareConfig: {
                     imageVersion: 'preview-debian10',
                 },
@@ -101,8 +59,8 @@ async function main(options: Options) {
                     }
                 },
                 workerConfig: {
-                    numInstances: 2,
-                    machineTypeUri: 'n1-standard-1',
+                    numInstances: 3,
+                    machineTypeUri: 'n1-standard-2',
                     diskConfig: {
                         bootDiskType: 'pd-standard',
                         bootDiskSizeGb: 500,
@@ -128,6 +86,7 @@ async function main(options: Options) {
         })
     }
 
+    console.log('Submitting InvertedIndex job ...');
     const [invIndexJobResp] = await jobClient.submitJob({
         projectId: options.projectId,
         region: options.region,
@@ -147,15 +106,9 @@ async function main(options: Options) {
             },
         },
     });
+    console.log(`Job submitted: ${invIndexJobResp.reference?.jobId}`);
 
-    console.log('Running InvertedIndex job ...');
-    const invIndexStatus = await waitForJob(options, jobClient, invIndexJobResp);
-    if (invIndexStatus !== 'DONE') {
-        console.log('InvertedIndex job didn\'t finish succesfully');
-        await deleteCluster(options, clusterClient);
-        return;
-    }
-
+    console.log('Submitting PageRank job ...');
     const [pageRankJobResp] = await jobClient.submitJob({
         projectId: options.projectId,
         region: options.region,
@@ -177,38 +130,18 @@ async function main(options: Options) {
             }
         }
     });
-
-    console.log('Running PageRank job ...');
-    const pageRankStatus = await waitForJob(options, jobClient, pageRankJobResp);
-    if (pageRankStatus !== 'DONE') {
-        console.log('PageRank job didn\'t finish succesfully');
-        await deleteCluster(options, clusterClient);
-        return;
-    }
-
-
-    const invIndexOutput = await storage
-        .bucket(DEFAULT_BUCKET)
-        .file(`${INVINDEX_OUTPUT}/part-r-00000`)
-        .download();
-
-    console.log(invIndexOutput);
-
-    await deleteCluster(options, clusterClient);
+    console.log(`Job submitted: ${pageRankJobResp.reference?.jobId}`);
 }
 
 const args = process.argv.slice(2);
-
-if (args.length >= 4) {
-    let timeout = (args.length === 5) ? +args[4] : 20;
+if (args.length === 4) {
     await main({
         projectId: args[0],
         region: args[1],
         clusterName: args[2],
         pageRankIterations: args[3],
-        timeout: timeout * 60 * 1000,
     });
 } else {
     console.log('Error: unexpected number of params');
-    console.log('Usage: ... <projectId> <region> <clusterName> <pagerank-iterations> <timeout?>');
+    console.log('Usage: ... <projectId> <region> <clusterName> <pagerank-iterations>');
 }
