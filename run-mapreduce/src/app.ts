73186bc0ee5328141355a22d4eea0d177024acea
Kevin Del Castillo <quebin31@gmail.com>
2020-10-28 15:00:13 -0500

    refactor: renamed script to run map reduce jobs

---
+++ run-mapreduce/src/app.ts
@@ -0,0 +1,120 @@
+import { ClusterControllerClient, JobControllerClient } from "@google-cloud/dataproc";
+import { Storage } from "@google-cloud/storage";
+
+interface Options {
+    projectId: string,
+    region: string,
+    clusterName: string,
+    pageRankIterations: string,
+    timeout: number,
+}
+
+function sleep(milliseconds: number) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+async function waitForJob(options: Options, jobClient: any, jobResp: any) {
+    const terminalStates = new Set(['DONE', 'ERROR', 'CANCELLED']);
+    const start = (new Date()).getMilliseconds();
+
+    const jobReq = {
+        projectId: options.projectId,
+        region: options.region,
+        jobId: jobResp.reference.jobId,
+    };
+
+    while (!terminalStates.has(jobResp.status.state)) {
+        let now = (new Date()).getMilliseconds();
+        if (now - options.timeout > start) {
+            await jobClient.cancelJob(jobReq);
+
+            console.log(`Job ${jobResp.reference.jobId} timed out after threshold of ${options.timeout / 60 * 1000} minutes`);
+        }
+
+        await sleep(1000);
+        [jobResp] = await jobClient.getJob(jobReq);
+    }
+}
+
+async function main(options: Options) {
+    console.log('Received the following options:', options);
+
+    const clusterClient = new ClusterControllerClient({
+        apiEndpoint: `${options.region}-dataproc.googleapis.com`,
+        projectId: options.projectId,
+    });
+
+    const jobClient = new JobControllerClient({
+        apiEndpoint: `${options.region}-dataproc.googleapis.com`,
+        projectId: options.projectId,
+    });
+
+    const [createOperation] = await clusterClient.createCluster({
+        projectId: options.projectId,
+        region: options.region,
+        cluster: {
+            clusterName: options.clusterName,
+            config: {
+                masterConfig: {
+                    numInstances: 1,
+                    machineTypeUri: 'n1-standard-1'
+                },
+                workerConfig: {
+                    numInstances: 2,
+                    machineTypeUri: 'n1-standard-1',
+                },
+            },
+        },
+    });
+
+    console.log(`Creating cluster: ${options.clusterName} ... (this may take some time)`);
+    const [createResponse] = await createOperation.promise();
+    console.log(`Cluster created successfully: ${createResponse.clusterName}`);
+
+    const [invIndexJobResp] = await jobClient.submitJob({
+        projectId: options.projectId,
+        region: options.region,
+        job: {
+            placement: {
+                clusterName: options.clusterName,
+            },
+            hadoopJob: {
+                mainClass: "InvertedIndex",
+                jarFileUris: [
+                    "gs://web-searcher-unsa-1/inv-index.jar"
+                ],
+                args: [
+                    "gs://web-searcher-unsa-1/web-offline",
+                    "gs://web-searcher-unsa-1/inv-index-output"
+                ]
+            },
+        },
+    });
+
+    const [deleteOperation] = await clusterClient.deleteCluster({
+        projectId: options.projectId,
+        region: options.region,
+        clusterName: options.clusterName,
+    });
+
+    console.log(`Deleting cluster`);
+    await deleteOperation.promise();
+
+    console.log(`Cluster deleted succesfully`);
+}
+
+const args = process.argv.slice(2);
+
+if (args.length >= 4) {
+    let timeout = (args.length === 5) ? +args[4] : 20;
+    await main({
+        projectId: args[0],
+        region: args[1],
+        clusterName: args[2],
+        pageRankIterations: args[3],
+        timeout: timeout * 60 * 1000,
+    });
+} else {
+    console.log("Error: unexpected number of params");
+    console.log("Usage: ... <projectId> <region> <clusterName> <pagerank-iterations> <timeout?>");
+}
