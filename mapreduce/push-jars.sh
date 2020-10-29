53abcc6db8d04449b03a574c7609af21ba0b32b8
Kevin Del Castillo <quebin31@gmail.com>
2020-10-28 20:20:18 -0500

    refactor: change default bucket

--- mapreduce/push-jars.sh
+++ mapreduce/push-jars.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+DEFAULT_BUCKET="web-searcher-cloud1"
+
 pushd inv-index
 make clean 
 JAVA8=1 make build 
-gsutil cp bin/main.jar gs://web-searcher-unsa-1/inv-index.jar
+gsutil cp bin/main.jar "gs://${DEFAULT_BUCKET}/inv-index.jar"
 popd 
 
 pushd page-rank
 make clean
 JAVA8=1 make build
-gsutil cp bin/main.jar gs://web-searcher-unsa-1/page-rank.jar
+gsutil cp bin/main.jar "gs://${DEFAULT_BUCKET}/page-rank.jar"
 popd 
--- submit-jobs/src/app.ts
+++ submit-jobs/src/app.ts
@@ -1,7 +1,7 @@
 import { ClusterControllerClient, JobControllerClient } from '@google-cloud/dataproc';
 import { Storage } from '@google-cloud/storage';
 
-const DEFAULT_BUCKET = 'web-searcher-unsa-1';
+const DEFAULT_BUCKET = 'web-searcher-cloud1';
 const WEB_OFFLINE = 'web-offline';
 const INVINDEX_OUTPUT = 'inv-index-output';
 const PAGERANK_TEMP = 'page-rank-temp';
@@ -12,6 +12,7 @@ interface Options {
     region: string,
     clusterName: string,
     pageRankIterations: string,
+    filter: String[],
 }
 
 
@@ -40,39 +41,51 @@ async function main(options: Options) {
         });
     }
 
-    const [createOperation] = await clusterClient.createCluster({
+    const [clusters] = await clusterClient.listClusters({
         projectId: options.projectId,
-        region: options.region,
-        cluster: {
-            clusterName: options.clusterName,
-            config: {
-                configBucket: configClusterBucket,
-                softwareConfig: {
-                    imageVersion: 'preview-debian10',
-                },
-                masterConfig: {
-                    numInstances: 1,
-                    machineTypeUri: 'n1-standard-1',
-                    diskConfig: {
-                        bootDiskType: 'pd-standard',
-                        bootDiskSizeGb: 500,
-                    }
-                },
-                workerConfig: {
-                    numInstances: 3,
-                    machineTypeUri: 'n1-standard-2',
-                    diskConfig: {
-                        bootDiskType: 'pd-standard',
-                        bootDiskSizeGb: 500,
-                    }
-                },
-            },
-        },
+        region: options.region
     });
 
-    console.log(`Creating cluster: ${options.clusterName} ... (this may take some time)`);
-    const [createResponse] = await createOperation.promise();
-    console.log(`Cluster created successfully: ${createResponse.clusterName}`);
+    if (!clusters.some((c) => c.clusterName == options.clusterName)) {
+        const [createOperation] = await clusterClient.createCluster({
+            projectId: options.projectId,
+            region: options.region,
+            cluster: {
+                clusterName: options.clusterName,
+                config: {
+                    configBucket: configClusterBucket,
+                    lifecycleConfig: {
+                        idleDeleteTtl: {
+                            seconds: 600,
+                        },
+                    },
+                    softwareConfig: {
+                        imageVersion: 'preview-debian10',
+                    },
+                    masterConfig: {
+                        numInstances: 1,
+                        machineTypeUri: 'n1-standard-1',
+                        diskConfig: {
+                            bootDiskType: 'pd-standard',
+                            bootDiskSizeGb: 500,
+                        }
+                    },
+                    workerConfig: {
+                        numInstances: 3,
+                        machineTypeUri: 'n1-standard-2',
+                        diskConfig: {
+                            bootDiskType: 'pd-standard',
+                            bootDiskSizeGb: 500,
+                        }
+                    },
+                },
+            },
+        });
+
+        console.log(`Creating cluster: ${options.clusterName} ... (this may take some time)`);
+        const [createResponse] = await createOperation.promise();
+        console.log(`Cluster created successfully: ${createResponse.clusterName}`);
+    }
 
     if (await storage.bucket(DEFAULT_BUCKET).file(INVINDEX_OUTPUT).exists()) {
         await storage.bucket(DEFAULT_BUCKET).deleteFiles({
@@ -86,62 +99,67 @@ async function main(options: Options) {
         })
     }
 
-    console.log('Submitting InvertedIndex job ...');
-    const [invIndexJobResp] = await jobClient.submitJob({
-        projectId: options.projectId,
-        region: options.region,
-        job: {
-            placement: {
-                clusterName: options.clusterName,
+    if (!options.filter.some((f) => f === 'inv-index')) {
+        console.log('Submitting InvertedIndex job ...');
+        const [invIndexJobResp] = await jobClient.submitJob({
+            projectId: options.projectId,
+            region: options.region,
+            job: {
+                placement: {
+                    clusterName: options.clusterName,
+                },
+                hadoopJob: {
+                    mainClass: 'InvertedIndex',
+                    jarFileUris: [
+                        `gs://${DEFAULT_BUCKET}/inv-index.jar`
+                    ],
+                    args: [
+                        `gs://${DEFAULT_BUCKET}/${WEB_OFFLINE}`,
+                        `gs://${DEFAULT_BUCKET}/${INVINDEX_OUTPUT}`
+                    ]
+                },
             },
-            hadoopJob: {
-                mainClass: 'InvertedIndex',
-                jarFileUris: [
-                    `gs://${DEFAULT_BUCKET}/inv-index.jar`
-                ],
-                args: [
-                    `gs://${DEFAULT_BUCKET}/${WEB_OFFLINE}`,
-                    `gs://${DEFAULT_BUCKET}/${INVINDEX_OUTPUT}`
-                ]
-            },
-        },
-    });
-    console.log(`Job submitted: ${invIndexJobResp.reference?.jobId}`);
+        });
+        console.log(`Job submitted: ${invIndexJobResp.reference?.jobId}`);
+    }
 
-    console.log('Submitting PageRank job ...');
-    const [pageRankJobResp] = await jobClient.submitJob({
-        projectId: options.projectId,
-        region: options.region,
-        job: {
-            placement: {
-                clusterName: options.clusterName,
-            },
-            hadoopJob: {
-                mainClass: 'PageRank',
-                jarFileUris: [
-                    `gs://${DEFAULT_BUCKET}/page-rank.jar`
-                ],
-                args: [
-                    `gs://${DEFAULT_BUCKET}/${WEB_OFFLINE}}]`,
-                    `gs://${DEFAULT_BUCKET}/${PAGERANK_TEMP}`,
-                    `gs://${DEFAULT_BUCKET}/${PAGERANK_OUTPUT}`,
-                    options.pageRankIterations.toString(),
-                ]
+    if (!options.filter.some((f) => f === 'page-rank')) {
+        console.log('Submitting PageRank job ...');
+        const [pageRankJobResp] = await jobClient.submitJob({
+            projectId: options.projectId,
+            region: options.region,
+            job: {
+                placement: {
+                    clusterName: options.clusterName,
+                },
+                hadoopJob: {
+                    mainClass: 'PageRank',
+                    jarFileUris: [
+                        `gs://${DEFAULT_BUCKET}/page-rank.jar`
+                    ],
+                    args: [
+                        `gs://${DEFAULT_BUCKET}/${WEB_OFFLINE}`,
+                        `gs://${DEFAULT_BUCKET}/${PAGERANK_TEMP}`,
+                        `gs://${DEFAULT_BUCKET}/${PAGERANK_OUTPUT}`,
+                        options.pageRankIterations.toString(),
+                    ]
+                }
             }
-        }
-    });
-    console.log(`Job submitted: ${pageRankJobResp.reference?.jobId}`);
+        });
+        console.log(`Job submitted: ${pageRankJobResp.reference?.jobId}`);
+    }
 }
 
 const args = process.argv.slice(2);
-if (args.length === 4) {
+if (args.length >= 5) {
     await main({
         projectId: args[0],
         region: args[1],
         clusterName: args[2],
         pageRankIterations: args[3],
+        filter: (args.length === 5) ? args[4].split(',') : []
     });
 } else {
     console.log('Error: unexpected number of params');
-    console.log('Usage: ... <projectId> <region> <clusterName> <pagerank-iterations>');
+    console.log('Usage: ... <projectId> <region> <clusterName> <pagerank-iterations> <filter>');
 }
