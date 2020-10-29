fcef5dc406e1c594ec6b091251ca9223206c1e4a
Kevin Del Castillo <quebin31@gmail.com>
2020-10-29 13:23:22 -0500

    fix: detection of ouput foldersfeat: use yargs to parseargs

--- submit-jobs/src/app.ts
+++ submit-jobs/src/app.ts
@@ -1,5 +1,6 @@
 import { ClusterControllerClient, JobControllerClient } from '@google-cloud/dataproc';
 import { Storage } from '@google-cloud/storage';
+import yargs from 'yargs';
 
 const DEFAULT_BUCKET = 'web-searcher-cloud1';
 const WEB_OFFLINE = 'web-offline';
@@ -7,56 +8,47 @@ const INVINDEX_OUTPUT = 'inv-index-output';
 const PAGERANK_TEMP = 'page-rank-temp';
 const PAGERANK_OUTPUT = 'page-rank-output';
 
-interface Options {
-    projectId: string,
-    region: string,
-    clusterName: string,
-    pageRankIterations: string,
-    filter: String[],
-}
-
-
-async function main(options: Options) {
-    console.log('Received the following options:', options);
+async function main(args: Arguments) {
+    console.log('Received the following arguments:', args);
 
     const clusterClient = new ClusterControllerClient({
-        apiEndpoint: `${options.region}-dataproc.googleapis.com`,
-        projectId: options.projectId,
+        apiEndpoint: `${args.region}-dataproc.googleapis.com`,
+        projectId: args.projectId,
     });
 
     const jobClient = new JobControllerClient({
-        apiEndpoint: `${options.region}-dataproc.googleapis.com`,
-        projectId: options.projectId,
+        apiEndpoint: `${args.region}-dataproc.googleapis.com`,
+        projectId: args.projectId,
     });
 
     const storage = new Storage();
-    const configClusterBucket = `${options.clusterName}-config`;
+    const configClusterBucket = `${args.clusterName}-config`;
     const buckets = await storage.getBuckets();
     if (!buckets[0].some((bucket) => bucket.name === configClusterBucket)) {
         console.log(`Creating config bucket ${configClusterBucket} for this cluster`);
         await storage.createBucket(configClusterBucket, {
-            location: options.region,
+            location: args.region,
             regional: true,
             standard: true,
         });
     }
 
     const [clusters] = await clusterClient.listClusters({
-        projectId: options.projectId,
-        region: options.region
+        projectId: args.projectId,
+        region: args.region
     });
 
-    if (!clusters.some((c) => c.clusterName == options.clusterName)) {
+    if (!clusters.some((c) => c.clusterName == args.clusterName)) {
         const [createOperation] = await clusterClient.createCluster({
-            projectId: options.projectId,
-            region: options.region,
+            projectId: args.projectId,
+            region: args.region,
             cluster: {
-                clusterName: options.clusterName,
+                clusterName: args.clusterName,
                 config: {
                     configBucket: configClusterBucket,
                     lifecycleConfig: {
                         idleDeleteTtl: {
-                            seconds: 600,
+                            seconds: 15 * 60,
                         },
                     },
                     softwareConfig: {
@@ -82,31 +74,62 @@ async function main(options: Options) {
             },
         });
 
-        console.log(`Creating cluster: ${options.clusterName} ... (this may take some time)`);
+        console.log(`Creating cluster: ${args.clusterName} ... (this may take some time)`);
         const [createResponse] = await createOperation.promise();
         console.log(`Cluster created successfully: ${createResponse.clusterName}`);
+    } else {
+        console.log(`Cluster ${args.clusterName} already exists`);
     }
 
-    if (await storage.bucket(DEFAULT_BUCKET).file(INVINDEX_OUTPUT).exists()) {
-        await storage.bucket(DEFAULT_BUCKET).deleteFiles({
-            directory: INVINDEX_OUTPUT,
-        });
+    const files = await storage.bucket(DEFAULT_BUCKET).getFiles();
+
+    if (files[0].some((f) => f.name == INVINDEX_OUTPUT)) {
+        console.log('InvertedIndex output exists');
+        if (args.force) {
+            console.log('Deleting InvertedIndex output');
+            await storage.bucket(DEFAULT_BUCKET).deleteFiles({
+                directory: INVINDEX_OUTPUT,
+            });
+        } else {
+            console.log('If you want to remove them run with \'--force\'');
+            return;
+        }
     }
 
-    if (await storage.bucket(DEFAULT_BUCKET).file(PAGERANK_OUTPUT).exists()) {
-        await storage.bucket(DEFAULT_BUCKET).deleteFiles({
-            directory: PAGERANK_OUTPUT
-        })
+    if (files[0].some((f) => f.name == PAGERANK_TEMP)) {
+        console.log('PageRank temp output exists');
+        if (args.force) {
+            console.log('Deleting PageRank temp output');
+            await storage.bucket(DEFAULT_BUCKET).deleteFiles({
+                directory: PAGERANK_TEMP
+            })
+        } else {
+            console.log('If you want to remove them run with \'--force\'');
+            return;
+        }
     }
 
-    if (!options.filter.some((f) => f === 'inv-index')) {
+    if (files[0].some((f) => f.name == PAGERANK_OUTPUT)) {
+        console.log('PageRank output exists');
+        if (args.force) {
+            console.log('Deleting PageRank output');
+            await storage.bucket(DEFAULT_BUCKET).deleteFiles({
+                directory: PAGERANK_OUTPUT
+            })
+        } else {
+            console.log('If you want to remove them run with \'--force\'');
+            return;
+        }
+    }
+
+    if (!args.filter.some((f) => f === 'inv-index')) {
         console.log('Submitting InvertedIndex job ...');
         const [invIndexJobResp] = await jobClient.submitJob({
-            projectId: options.projectId,
-            region: options.region,
+            projectId: args.projectId,
+            region: args.region,
             job: {
                 placement: {
-                    clusterName: options.clusterName,
+                    clusterName: args.clusterName,
                 },
                 hadoopJob: {
                     mainClass: 'InvertedIndex',
@@ -121,16 +144,18 @@ async function main(options: Options) {
             },
         });
         console.log(`Job submitted: ${invIndexJobResp.reference?.jobId}`);
+    } else {
+        console.log('InvertedIndex job was filtered');
     }
 
-    if (!options.filter.some((f) => f === 'page-rank')) {
+    if (!args.filter.some((f) => f === 'page-rank')) {
         console.log('Submitting PageRank job ...');
         const [pageRankJobResp] = await jobClient.submitJob({
-            projectId: options.projectId,
-            region: options.region,
+            projectId: args.projectId,
+            region: args.region,
             job: {
                 placement: {
-                    clusterName: options.clusterName,
+                    clusterName: args.clusterName,
                 },
                 hadoopJob: {
                     mainClass: 'PageRank',
@@ -141,25 +166,34 @@ async function main(options: Options) {
                         `gs://${DEFAULT_BUCKET}/${WEB_OFFLINE}`,
                         `gs://${DEFAULT_BUCKET}/${PAGERANK_TEMP}`,
                         `gs://${DEFAULT_BUCKET}/${PAGERANK_OUTPUT}`,
-                        options.pageRankIterations.toString(),
+                        args.pageRankIters.toString(),
                     ]
                 }
             }
         });
         console.log(`Job submitted: ${pageRankJobResp.reference?.jobId}`);
+    } else {
+        console.log('PageRank job was filtered');
     }
 }
 
-const args = process.argv.slice(2);
-if (args.length >= 4) {
-    await main({
-        projectId: args[0],
-        region: args[1],
-        clusterName: args[2],
-        pageRankIterations: args[3],
-        filter: (args.length === 5) ? args[4].split(',') : []
-    });
-} else {
-    console.log('Error: unexpected number of params');
-    console.log('Usage: ... <projectId> <region> <clusterName> <pagerank-iterations> <filter>');
+interface Arguments {
+    [x: string]: unknown,
+    projectId: string,
+    region: string,
+    clusterName: string,
+    pageRankIters: number,
+    filter: string[],
+    force: boolean,
 }
+
+const args: Arguments = yargs(process.argv.slice(2)).options({
+    force: { type: 'boolean', default: false },
+    projectId: { type: 'string', alias: 'p', demandOption: true },
+    region: { type: 'string', alias: 'r', demandOption: true },
+    clusterName: { type: 'string', alias: 'c', demandOption: true },
+    pageRankIters: { type: 'number', alias: 'i', default: 10 },
+    filter: { type: 'array', alias: 'f', default: [] },
+}).argv;
+
+await main(args);
