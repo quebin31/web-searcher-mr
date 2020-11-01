1d7941cc567fece7f5518de6230bda221359098e
Kevin Del Castillo <quebin31@gmail.com>
2020-11-01 15:05:27 -0500

    fix: testing to wait for streams to finish

--- firestore/src/app.ts
+++ firestore/src/app.ts
@@ -1,17 +1,32 @@
-import { Storage } from '@google-cloud/storage';
 import { Firestore, FieldValue, CollectionReference } from '@google-cloud/firestore';
+import { Storage, File } from '@google-cloud/storage';
+import { customAlphabet } from 'nanoid';
+import { pipeline } from 'stream';
 
 import estream from 'event-stream';
 import yargs from 'yargs';
 
 const DEFAULT_BUCKET = 'web-searcher-cloud1';
-const INVINDEX_OUTPUT = 'inv-index-output';
-const PAGERANK_OUTPUT = 'page-rank-output';
+const INVINDEX_OUTPUT = 'inv-index-output-test';
+const PAGERANK_OUTPUT = 'page-rank-output-test';
 const OUT_PART_REGEX = /part-r-[0-9]*$/;
+const VALID_WORD_REGEX = /[a-zA-Z0-9]+/;
 
 interface Map {
     [x: string]: string,
 }
+
+interface Arguments {
+    [x: string]: unknown,
+    projectId: string
+}
+
+interface Files {
+    invIndexFiles: File[],
+    pageRankFiles: File[],
+}
+
+const generateId = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 14);
 
 async function getStoredWebsites(firestore: Firestore): Promise<Map> {
     const query = await firestore.collection('websites').get();
@@ -27,110 +42,136 @@ async function getStoredWebsites(firestore: Firestore): Promise<Map> {
     return websites;
 }
 
-async function storeWebsite(websiteUrl: string, websites: Map, collection: CollectionReference) {
-    const doc = await collection.add({
-        'url': websiteUrl
-    });
+async function maybeStoreWebsite(websiteUrl: string, websites: Map, collection: CollectionReference) {
+    if (websiteUrl in websites) {
+        return;
+    }
 
-    websites[websiteUrl] = doc.id;
+    const id = generateId();
+    await collection.doc(id).set({ 'url': websiteUrl });
+    websites[websiteUrl] = id;
 }
 
-async function storeInvIndex(websites: Map, storage: Storage, firestore: Firestore) {
-    const invIndexFiles = await storage.bucket(DEFAULT_BUCKET).getFiles({
-        directory: INVINDEX_OUTPUT,
-    });
+function invIndexStep(files: Files, websites: Map, firestore: Firestore, storage: Storage) {
+    const file = files.invIndexFiles.shift();
+    if (file === undefined) {
+        pageRankStep(files, websites, firestore, storage);
+        return;
+    }
+
+    if (!OUT_PART_REGEX.test(file.name)) {
+        invIndexStep(files, websites, firestore, storage);
+        return;
+    }
+
+    console.log(`inv-index: processing file: ${file.name}`);
 
     const websitesCollection = firestore.collection('websites');
     const invIndexCollection = firestore.collection('invIndex');
 
-    for (const file of invIndexFiles[0]) {
-        if (!OUT_PART_REGEX.test(file.name)) continue;
+    const stream = storage.bucket(DEFAULT_BUCKET).file(file.name).createReadStream();
 
-        const stream = await storage.bucket(DEFAULT_BUCKET).file(file.name).createReadStream();
+    stream
+        .on('end', () => invIndexStep(files, websites, firestore, storage))
+        .pipe(estream.split())
+        .pipe(estream.map(async (line: string) => {
+            stream.pause();
 
-        stream.pipe(estream.split()).pipe(estream.map(async (line: string) => {
             const keyValue = line.split('\t');
             const [word, urls] = [keyValue[0], keyValue[1]];
 
-            const wordDoc = invIndexCollection.doc(word);
-
-            for (const url of urls.split('|')) {
-                const websiteStored = url in websites;
-                if (!websiteStored) await storeWebsite(url, websites, websitesCollection);
-
-                const websiteId = websites[url];
-                wordDoc.set({
-                    'websites': FieldValue.arrayUnion(websiteId),
-                }, { merge: true });
+            if (!VALID_WORD_REGEX.test(word)) {
+                stream.resume();
+                return;
             }
 
+            const wordDoc = invIndexCollection.doc(word);
+
+            let batch = firestore.batch();
+            let writes = 0;
+            for (const url of urls.split('|')) {
+                await maybeStoreWebsite(url, websites, websitesCollection);
+                const websiteId = websites[url];
+
+                batch.set(wordDoc, { 'websites': FieldValue.arrayUnion(websiteId) }, { merge: true });
+                writes += 1;
+
+                if (writes === 500) {
+                    await batch.commit();
+                    writes = 0;
+                    batch = firestore.batch();
+                }
+            }
+
+            await batch.commit();
+            stream.resume();
         }));
-    }
 }
 
-async function storePageRank(websites: Map, storage: Storage, firestore: Firestore) {
-    const pageRankFiles = await storage.bucket(DEFAULT_BUCKET).getFiles({
-        directory: PAGERANK_OUTPUT
-    });
+function pageRankStep(files: Files, websites: Map, firestore: Firestore, storage: Storage) {
+    const file = files.pageRankFiles.shift();
+    if (file === undefined) return;
+
+    if (!OUT_PART_REGEX.test(file.name)) {
+        pageRankStep(files, websites, firestore, storage);
+        return;
+    }
+
+    console.log(`page-rank: processing file: ${file.name}`);
 
     const websitesCollection = firestore.collection('websites');
     const pageRankCollection = firestore.collection('pageRank');
 
-    for (const file of pageRankFiles[0]) {
-        if (!OUT_PART_REGEX.test(file.name)) continue;
+    const stream = storage.bucket(DEFAULT_BUCKET).file(file.name).createReadStream();
 
-        console.log(`Processing file ${file.name}`);
-
-        const stream = storage.bucket(DEFAULT_BUCKET).file(file.name).createReadStream();
-
-        stream.pipe(estream.split()).pipe(estream.map(async (line: string) => {
+    stream
+        .on('end', () => pageRankStep(files, websites, firestore, storage))
+        .pipe(estream.split())
+        .pipe(estream.map(async (line: string) => {
             stream.pause();
 
             const key = line.split('\t')[0];
 
             const urlAndPageRank = key.split('|');
             const [url, pageRank] = [urlAndPageRank[0], +urlAndPageRank[1]];
-            console.log(`${url}: ${pageRank}`);
 
-            /*
             const websitedStored = url in websites;
-            if (!websitedStored) await storeWebsite(url, websites, websitesCollection);
+            if (!websitedStored) await maybeStoreWebsite(url, websites, websitesCollection);
 
             const websiteId = websites[url];
             const pageRankDoc = pageRankCollection.doc(websiteId);
             pageRankDoc.set({ value: pageRank }, { merge: true });
-            */
 
             stream.resume();
         }));
-    }
 }
 
 async function main(args: Arguments) {
-    const storage = new Storage({
-        projectId: args.projectId,
-    });
-
-    const firestore = new Firestore({
-        projectId: args.projectId,
-    });
-
+    const storage = new Storage({ projectId: args.projectId });
+    const firestore = new Firestore({ projectId: args.projectId });
     const websites = await getStoredWebsites(firestore);
 
-    // console.log('Storing InvertedIndex output values');
-    // await storeInvIndex(websites, storage, firestore);
+    const invIndexFilesResponse = await storage.bucket(DEFAULT_BUCKET).getFiles({
+        directory: INVINDEX_OUTPUT,
+    });
 
-    console.log('Storing PageRank output values');
-    await storePageRank(websites, storage, firestore);
+    const pageRankFilesResponse = await storage.bucket(DEFAULT_BUCKET).getFiles({
+        directory: PAGERANK_OUTPUT
+    });
+
+    const files: Files = {
+        invIndexFiles: invIndexFilesResponse[0],
+        pageRankFiles: pageRankFilesResponse[0]
+    };
+
+    invIndexStep(files, websites, firestore, storage);
+    pageRankStep(files, websites, firestore, storage);
 }
 
-interface Arguments {
-    [x: string]: unknown,
-    projectId: string
-}
-
-const args: Arguments = yargs(process.argv.slice(2)).options({
-    projectId: { type: 'string', alias: 'p', demandOption: true },
-}).argv;
-
-await main(args);
+await main(
+    yargs(process.argv.slice(2))
+        .options({
+            projectId: { type: 'string', alias: 'p', demandOption: true },
+        })
+        .argv
+);
