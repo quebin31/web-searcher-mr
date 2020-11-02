2f8257903ec00de881481245926c416f12198a1b
Kevin Del Castillo <quebin31@gmail.com>
2020-11-02 12:50:58 -0500

    feat: possibly working version, commit to store changes

--- build-firestore/src/app.ts
+++ build-firestore/src/app.ts
@@ -1,15 +1,16 @@
 import { Firestore, FieldValue, CollectionReference } from '@google-cloud/firestore';
 import { Storage, File } from '@google-cloud/storage';
 import { customAlphabet } from 'nanoid';
+import { Mutex } from 'async-mutex';
 
-import estream from 'event-stream';
+import es from 'event-stream';
 import yargs from 'yargs';
 
 const DEFAULT_BUCKET = 'web-searcher-cloud1';
-const INVINDEX_OUTPUT = 'inv-index-out-test';
-const PAGERANK_OUTPUT = 'page-rank-out-test';
+const INVINDEX_OUTPUT = 'inv-index-output';
+const PAGERANK_OUTPUT = 'page-rank-output';
 const OUT_PART_REGEX = /part-r-[0-9]*$/;
-const VALID_WORD_REGEX = /[a-zA-Z0-9-]+/;
+const VALID_WORD_REGEX = /[a-zA-Z0-9][a-zA-Z0-9-]*/;
 
 interface Map {
     [x: string]: string,
@@ -25,6 +26,7 @@ interface Files {
     pageRankFiles: File[],
 }
 
+const mutex = new Mutex();
 const generateId = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 14);
 
 async function getStoredWebsites(firestore: Firestore): Promise<Map> {
@@ -41,24 +43,39 @@ async function getStoredWebsites(firestore: Firestore): Promise<Map> {
     return websites;
 }
 
-async function maybeStoreWebsite(websiteUrl: string, websites: Map, collection: CollectionReference) {
-    if (websiteUrl in websites) {
-        return;
+async function getWebsiteId(websiteUrl: string, websites: Map, collection: CollectionReference, options: { create: boolean }): Promise<string | undefined> {
+    const release = await mutex.acquire();
+    let websiteId;
+
+    try {
+        if (websiteUrl in websites) {
+            websiteId = websites[websiteUrl];
+        } else if (options.create) {
+            websiteId = generateId();
+            await collection.doc(websiteId).set({ 'url': websiteUrl });
+            websites[websiteUrl] = websiteId;
+        }
+    } catch (e) {
+        console.log(`Error getting/creating website id for ${websiteUrl} (${e})`);
+        throw e;
+    } finally {
+        release();
     }
 
-    const id = generateId();
-    await collection.doc(id).set({ 'url': websiteUrl });
-    websites[websiteUrl] = id;
+    return websiteId;
 }
 
 function invIndexStep(files: Files, websites: Map, firestore: Firestore, storage: Storage) {
+    console.log(`inv-index: files remaining: ${files.invIndexFiles.length}`);
     const file = files.invIndexFiles.shift();
     if (file === undefined) {
+        console.log('inv-index: stepping into pagerank');
         pageRankStep(files, websites, firestore, storage);
         return;
     }
 
     if (!OUT_PART_REGEX.test(file.name)) {
+        console.log(`inv-index: skipping file: ${file.name}`);
         invIndexStep(files, websites, firestore, storage);
         return;
     }
@@ -68,29 +85,35 @@ function invIndexStep(files: Files, websites: Map, firestore: Firestore, storage
     const websitesCollection = firestore.collection('websites');
     const invIndexCollection = firestore.collection('inv-index');
 
-    const stream = storage.bucket(DEFAULT_BUCKET).file(file.name).createReadStream();
+    const fileStream = storage.bucket(DEFAULT_BUCKET).file(file.name).createReadStream();
 
-    stream
-        .on('end', () => invIndexStep(files, websites, firestore, storage))
-        .pipe(estream.split())
-        .pipe(estream.map(async (line: string) => {
-            stream.pause();
+
+    fileStream
+        .on('error', (e) => console.log(`inv-index: error stream: ${e}`))
+        .on('close', () => {
+            console.log(`inv-index: closed stream: ${file.name}`);
+            invIndexStep(files, websites, firestore, storage);
+        })
+        .pipe(es.split())
+        .pipe(es.mapSync(async (line: string) => {
+            fileStream.pause();
 
             const keyValue = line.split('\t');
             const [word, urls] = [keyValue[0], keyValue[1]];
 
             if (!VALID_WORD_REGEX.test(word)) {
-                stream.resume();
+                fileStream.resume();
                 return;
             }
+
+            console.log(`inv-index: processing word: ${word}`);
 
             const wordDoc = invIndexCollection.doc(word);
 
             let batch = firestore.batch();
             let writes = 0;
             for (const url of urls.split('|')) {
-                await maybeStoreWebsite(url, websites, websitesCollection);
-                const websiteId = websites[url];
+                const websiteId = await getWebsiteId(url, websites, websitesCollection, { create: true });
 
                 batch.set(wordDoc, { 'websites': FieldValue.arrayUnion(websiteId) }, { merge: true });
                 writes += 1;
@@ -103,15 +126,21 @@ function invIndexStep(files: Files, websites: Map, firestore: Firestore, storage
             }
 
             await batch.commit();
-            stream.resume();
+
+            console.log(`inv-index: finished word: ${word}`);
+
+            fileStream.resume();
         }));
 }
 
+
 function pageRankStep(files: Files, websites: Map, firestore: Firestore, storage: Storage) {
+    console.log(`page-rank: files remaining: ${files.pageRankFiles.length}`);
     const file = files.pageRankFiles.shift();
     if (file === undefined) return;
 
     if (!OUT_PART_REGEX.test(file.name)) {
+        console.log(`page-rank: skipping file: ${file.name}`);
         pageRankStep(files, websites, firestore, storage);
         return;
     }
@@ -121,27 +150,32 @@ function pageRankStep(files: Files, websites: Map, firestore: Firestore, storage
     const websitesCollection = firestore.collection('websites');
     const pageRankCollection = firestore.collection('page-rank');
 
-    const stream = storage.bucket(DEFAULT_BUCKET).file(file.name).createReadStream();
+    const fileStream = storage.bucket(DEFAULT_BUCKET).file(file.name).createReadStream();
 
-    stream
-        .on('end', () => pageRankStep(files, websites, firestore, storage))
-        .pipe(estream.split())
-        .pipe(estream.map(async (line: string) => {
-            stream.pause();
+    fileStream
+        .on('error', (e) => console.log(`page-rank: error stream: ${e}`))
+        .on('close', () => {
+            console.log(`page-rank: closed stream: ${file.name}`);
+            pageRankStep(files, websites, firestore, storage);
+        })
+        .pipe(es.split())
+        .pipe(es.mapSync(async (line: string) => {
+            fileStream.pause();
 
             const key = line.split('\t')[0];
 
             const urlAndPageRank = key.split('|');
             const [url, pageRank] = [urlAndPageRank[0], +urlAndPageRank[1]];
 
-            const websitedStored = url in websites;
-            if (!websitedStored) await maybeStoreWebsite(url, websites, websitesCollection);
+            const websiteId = await getWebsiteId(url, websites, websitesCollection, { create: false });
+            if (websiteId !== undefined) {
+                console.log(`page-rank: processing url: ${url}`);
+                const pageRankDoc = pageRankCollection.doc(websiteId);
+                pageRankDoc.set({ value: pageRank }, { merge: true });
+                console.log(`page-rank: finished url: ${url}`);
+            }
 
-            const websiteId = websites[url];
-            const pageRankDoc = pageRankCollection.doc(websiteId);
-            pageRankDoc.set({ value: pageRank }, { merge: true });
-
-            stream.resume();
+            fileStream.resume();
         }));
 }
 
