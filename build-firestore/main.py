bf88af8100a383893aadc3a4060cb673e80bf72d
Kevin Del Castillo <quebin31@gmail.com>
2020-11-02 20:24:35 -0500

    feat: manage batch operations automatically with AutoBatch. feat: manage websites with Websites and AutoBatch. feat: added halo spinners

--- build-firestore/main.py
+++ build-firestore/main.py
@@ -1,8 +1,8 @@
+# cython: language_level=3
+
 import argparse
 import re
 
-
-from tqdm import tqdm
 from os import path, makedirs
 from typing import Mapping
 from nanoid import generate
@@ -10,50 +10,91 @@ from google.cloud.storage import Client as Storage
 from google.cloud.storage.blob import Blob
 from google.cloud.firestore import Client as Firestore
 from google.cloud.firestore import ArrayUnion
-
+from google.cloud.firestore import DocumentReference
+from halo import Halo
 
 DEFAULT_BUCKET = 'web-searcher-cloud1'
-INVINDEX_OUTPUT = 'inv-index-output'
-PAGERANK_OUTPUT = 'page-rank-output'
+INVINDEX_OUTPUT = 'inv-index-out-test'
+PAGERANK_OUTPUT = 'page-rank-out-test'
 VALID_PART = re.compile('.*/part-r-[0-9]*$')
 VALID_WORD = re.compile('^[a-zA-Z0-9][a-zA-Z0-9-]*$')
 DOWNLOADS = 'downloads'
+BATCH_MAX_SIZE = 500
 
 
 def generate_id():
     return generate('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 14)
 
 
-def get_website_id(website: str, websites: Mapping[str, str], db: Firestore, create: bool):
+class AutoBatch:
+    def __init__(self, db: Firestore):
+        self.db = db
+        self.batch = db.batch()
+        self.bsize = 0
+
+    def create(self, ref: DocumentReference, data: any):
+        self.batch.create(ref, data)
+        self.bsize += 1
+
+        if self.bsize == BATCH_MAX_SIZE:
+            self.batch.commit()
+            self.batch = self.db.batch()
+            self.bsize = 0
+
+    def finish(self):
+        if self.bsize != 0:
+            self.batch.commit()
+
+
+class Websites:
+    def __init__(self, db: Firestore):
+        self.batch = AutoBatch(db)
+        self.webids: Mapping[str, str] = {}
+
+    def __getitem__(self, key: str):
+        return self.webids.__getitem__(key)
+
+    def __contains__(self, key: str):
+        return self.webids.__contains__(key)
+
+    def finish(self):
+        self.batch.finish()
+
+    def store(self, website: str):
+        webid = generate_id()
+        website_doc = db.collection('websites').document(webid)
+        self.batch.create(website_doc, {'url': website})
+        self.webids[website] = webid
+
+        return webid
+
+
+def get_website_id(website: str, websites: Websites, create: bool):
     if website in websites:
         return websites[website]
     elif create:
-        website_id = generate_id()
-        website_doc = db.collection('websites').document(website_id)
-        website_doc.set({
-            'url': website
-        })
-
-        websites[website] = website_id
-        return website_id
+        return websites.store(website)
     else:
         return None
 
 
 def maybe_download_blob(blob: Blob, storage: Storage, prefix):
+    spinner = Halo(text=f'{prefix}: downloading file: {blob.name}')
+    spinner.start()
+
     if not path.exists(path.join(DOWNLOADS, blob.name)):
-        print(f'{prefix}: downloading file: {blob.name}')
         makedirs(path.join(DOWNLOADS, path.dirname(blob.name)), exist_ok=True)
         blob.download_to_filename(path.join(DOWNLOADS, blob.name))
+        spinner.succeed(f'{prefix}: successfully downloaded: {blob.name}')
     else:
-        print(f'{prefix}: file already downloaded: {blob.name}')
+        spinner.succeed(f'{prefix}: file already downloaded: {blob.name}')
 
 
 def open_downloaded_blob(blob: Blob):
     return open(path.join(DOWNLOADS, blob.name), mode='r')
 
 
-def push_inv_index(websites: Mapping[str, str], db: Firestore, storage: Storage):
+def push_inv_index(websites: Websites, db: Firestore, storage: Storage):
     blobs = storage.list_blobs(DEFAULT_BUCKET, prefix=INVINDEX_OUTPUT)
     inv_index_collection = db.collection('inv-index')
 
@@ -62,28 +103,34 @@ def push_inv_index(websites: Mapping[str, str], db: Firestore, storage: Storage)
             continue
 
         maybe_download_blob(blob, storage, prefix='inv-index')
+
+        spinner = Halo(text=f'inv-index: processing blob: {blob.name}')
+        spinner.start()
+
         with open_downloaded_blob(blob) as file:
-            print(f'inv-index: processing file: {blob.name}')
+            batch = AutoBatch(db)
+
             for line in file:
+                line = line.replace('\n', '')
                 key_value = line.split('\t')
                 word, urls = key_value[0], key_value[1]
 
                 if not VALID_WORD.match(word):
                     continue
 
-                print(f'inv-index: processing word: {word}', end='\x1b[1K\r')
-                websites_ids = []
-
-                for url in urls.split('|'):
-                    website_id = get_website_id(url, websites, db, create=True)
-                    websites_ids.append(website_id)
+                urls = urls.split('|')
+                websites_ids = [get_website_id(
+                    url, websites, create=True) for url in urls]
 
                 word_doc = inv_index_collection.document(word)
-                word_doc.set(
-                    {'websites': ArrayUnion(websites_ids)}, merge=True)
+                batch.create(word_doc, {'websites': ArrayUnion(websites_ids)})
+
+            batch.finish()
+
+        spinner.succeed(f'inv-index: successfully processed: {blob.name}')
 
 
-def push_page_rank(websites: Mapping[str, str], db: Firestore, storage: Storage):
+def push_page_rank(websites: Websites, db: Firestore, storage: Storage):
     blobs = storage.list_blobs(DEFAULT_BUCKET, prefix=PAGERANK_OUTPUT)
     page_rank_collection = db.collection('page-rank')
 
@@ -92,19 +139,27 @@ def push_page_rank(websites: Mapping[str, str], db: Firestore, storage: Storage)
             continue
 
         maybe_download_blob(blob, storage, prefix='page-rank')
+
+        spinner = Halo(text=f'page-rank: processing blob: {blob.name}')
+        spinner.start()
         with open_downloaded_blob(blob) as file:
-            print(f'page-rank: processing file: {blob.name}')
+            batch = AutoBatch(db)
+
             for line in file:
+                line = line.replace('\n', '')
                 key = line.split('\t')[0]
                 url_and_pr = key.split('|')
-                url, pr = url_and_pr[0], float(url_and_pr[1])
+                url = url_and_pr[0]
 
-                website_id = get_website_id(url, websites, db, create=False)
+                website_id = get_website_id(url, websites, create=False)
+                if website_id is not None:
+                    pr = float(url_and_pr[1])
+                    pr_doc = page_rank_collection.document(website_id)
+                    batch.create(pr_doc, {'value': pr})
 
-                if website is not None:
-                    print(f'page-rank: processing url: {url}', end='\x1b[1K\r')
-                    pr_doc = page_rank_collection.doc(website_id)
-                    pr_doc.set({'value': pr}, merge=True)
+            batch.finish()
+
+        spinner.succeed(f'page-rank: successfully processed: {blob.name}')
 
 
 if __name__ == '__main__':
@@ -113,11 +168,12 @@ if __name__ == '__main__':
                         required=True, help='Project id')
 
     args = parser.parse_args()
-    print(args.project_id)
+    print(f'Project: {args.project_id}')
 
     db = Firestore(project=args.project_id)
     storage = Storage(project=args.project_id)
-    websites = {}
+    websites = Websites(db)
 
     push_inv_index(websites, db, storage)
     push_page_rank(websites, db, storage)
+    websites.finish()
